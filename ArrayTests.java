d6bbc9dc85bd00040a103bfa3cd026faafd2ee37
BernicoJC <108056736+BernicoJC@users.noreply.github.com>
2023-04-20 16:46:22 -0700

    checkpoint 1

--- ArrayTests.java
+++ ArrayTests.java
@@ -5,14 +5,23 @@ public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
     int[] input1 = { 3 };
+    int[] input2 = { 3, 2, 1 };
+    int[] input3 = { };
     ArrayExamples.reverseInPlace(input1);
+    ArrayExamples.reverseInPlace(input2);
+    ArrayExamples.reverseInPlace(input3);
+  
     assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(new int[]{1, 2, 3}, input2);
+    assertArrayEquals(new int[]{}, input3);
 	}
 
 
   @Test
   public void testReversed() {
     int[] input1 = { };
+    int[] input2 = {1, 2, 3};
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    assertArrayEquals(new int[]{3, 2, 1}, ArrayExamples.reversed(input2));
   }
 }
